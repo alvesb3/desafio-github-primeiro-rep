1d6f618eedc339147f2331ad2a819f88135e2376
Alvesb3 <davialvesskinny@gmail.com>
2022-01-21 17:38:51 -0300

    Atualização dos arquivos.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello World!")
+console.log("Hello World!\n Em duas linhas!")
+//* Para declarar um texto comum, basta declarar dentro das aspas. o comando \n quebra uma linha.
+console.log("Estou afim de "  + `${5*3}` + " Reais, para comprar um bacon.")
+//*Para Inserir uma operação dentro do console.log, é necessário declarar a sentença usando ``.
+console.log(5+3)
+//* Para inserir uma operação, basta usar o operador aritmético referente a operação que você deseja realizar. (Tabela dos operadores disponível na Apostila de JavaScript.)
